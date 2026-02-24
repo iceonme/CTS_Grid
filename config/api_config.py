cef264056d1ecf15f18bec4841781f2d1ac3e7c0
iceonme <iceonmel@gmail.com>
2026-02-24 19:45:07 +0800

    Include API config for deployment as requested

---
+++ config/api_config.py
@@ -0,0 +1,17 @@
+"""
+API 配置文件
+从原项目迁移的 OKX API 配置
+"""
+
+# OKX API 配置（模拟盘）
+# 原配置来自 readme.md / run_live_grid.py
+OKX_DEMO_CONFIG = {
+    'api_key': '72aac042-9859-48ec-8e27-9722524429a6',
+    'api_secret': 'CCFE2963EBD154027557D24CFA2CAA57',
+    'passphrase': 'Tonghua9527_',
+    'is_demo': True
+}
+
+# 默认交易配置
+DEFAULT_SYMBOL = 'BTC-USDT'
+DEFAULT_TIMEFRAME = '1m'
